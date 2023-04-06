0d37a23e3d58a741230cecf16c35a96e6df798e0
anfateeva <129980964+anfateeva@users.noreply.github.com>
2023-04-06 17:01:37 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as sts
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 727034582 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
