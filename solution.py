0596583428268e4cf58c342f23a1188f60da0194
anfateeva <129980964+anfateeva@users.noreply.github.com>
2023-04-06 22:12:31 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,5 +8,5 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    error = sts.laplace(1, 0).rvs(size = x.size)
-    return ((np.mean(x) + np.mean(error))/882) # Ваш ответ
+    return(np.mean(x)/882) # Ваш ответ
+
